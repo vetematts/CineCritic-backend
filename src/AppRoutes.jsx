188d6049c6d6c4bda19927e1013d2fa7f4ecb126
vetematts <165625991+vetematts@users.noreply.github.com>
2025-12-20 13:55:40 +1000

    Add basic AppRoutes

---
+++ src/AppRoutes.jsx
@@ -0,0 +1,16 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Movie from './pages/Movie.jsx'
+import Login from './pages/Login.jsx'
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/movie" element={<Movie />} />
+      <Route path="/login" element={<Login />} />
+    </Routes>
+  )
+}
+
+export default AppRoutes
