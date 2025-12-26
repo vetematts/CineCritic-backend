7f348ec68e07c588e34433a12d3a894d2ad07d32
vetematts <165625991+vetematts@users.noreply.github.com>
2025-12-27 01:24:49 +1000

    chore: restructure server files and update import paths

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,5 @@
-import app from './app/app.js';
-import { config } from './config/index.js';
+import app from './app.js';
+import { config } from '../config/index.js';
 
 app.listen(config.port, () => {
   console.log(`API listening on port ${config.port}`);
