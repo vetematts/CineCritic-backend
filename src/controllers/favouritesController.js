67240e42e446c399c0db670f5a62d1bc85516556
macfluffy <shadowimitation@hotmail.com>
2026-01-20 14:46:57 +1100

    feat: Added the deleteFavouritesHandler. Checks that argument sent for userId and movieId are valid types and exist before deleting. Throws an error if no movie was deleted from not being in a user's favourites list.
    Changed the import to include the .js file extension specifically because of package.json type set to module.

--- src/controllers/favouritesController.js
+++ src/controllers/favouritesController.js
@@ -1,8 +1,9 @@
-import { getContentById, getPosterUrl } from '../services/tmdb.js';
-import { upsertMovie, getMovieIdByTmdbId } from '../models/movies.js';
-import { BadRequestError, ForbiddenError, NotFoundError } from '../errors/http.js';
-import { addToFavourites, getFavourites } from '../models/favourites.js';
+// Import Middlewares
+import { ForbiddenError, NotFoundError } from '../errors/http.js';
 import { ensureMovieId } from './watchlistController.js';
+
+// Import Favourites CRUD methods to the PSQL database
+import { addToFavourites, getFavourites, removeFromFavourites } from '../models/favourites.js';
 
 // Checks you are the user before adding a movie to your favourites list
 export async function addToFavouritesHandler(req, res) {
@@ -35,4 +36,19 @@ export async function getFavouritesHandler(req, res) {
   // Get the favourites of the user
   const favourites = await getFavourites(targetId);
   res.json(favourites); // Return all the favourite movies as a JSON response
+}
+
+// Checks you are the user before removing a movie from your favourites
+export async function deleteFavouritesHandler(req, res) {
+    const { userId, tmdbId } = req.validated.params;    // Unpackage the userID from the request body
+    const targetUserId = Number(userId);                // Convert the userID into a number
+    const targetMovieId = ensureMovieId(tmdbId);        // Check this movie exists
+
+    // Attempt to remove the movie from user's favourites
+    const deletedEntry = await removeFromFavourites(targetUserId, targetMovieId);
+    if (!deletedEntry) {
+        throw new NotFoundError("This movie is not on your Favourite's list");
+    }
+
+    res.status(204).send(); // 204 No Content - Successfully removed movie
 }
